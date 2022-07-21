f0144b838303463f86ab907ef7e2b2712132663b
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-07-21 13:22:22 +0100

    commit

--- server/src/resources/user/user.validation.ts
+++ server/src/resources/user/user.validation.ts
@@ -1,5 +1,21 @@
 import Joi from "joi";
 
 const register = Joi.object({
-    name: Joi.string().max(30).required()
+
+    name: Joi.string().max(30).required(),
+
+    email: Joi.string().email().required(),
+
+    password: Joi.string().min(6).required()
+
 })
+
+const login = Joi.object({
+
+    email: Joi.string().email().required(),
+
+    password: Joi.string().min(6).required()
+
+})
+
+export default { register, login }
